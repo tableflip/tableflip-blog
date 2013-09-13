3e8365d228705d8f8f63dea7a24647a96624c4d5
Alan Shaw <alan@freestyle-developments.co.uk>
2013-09-13 11:35:28 +0100

    Send the mail

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 jQuery(function ($) {
   $("#main").css("min-height", $(window).height())
+  
+  if (window.location.hash == "#sent") {
+    $("#hire-us").hide()
+  } else {
+    $("#sent").hide()
+  }
 })
