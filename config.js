0c7194d9550a0572c7d6f64dc074db1326a6dab6
Alan Shaw <alan@freestyle-developments.co.uk>
2014-01-22 12:34:05 +0000

    Configure site URL

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://tableflip.io',
         mail: {},
         database: {
             client: 'sqlite3',
