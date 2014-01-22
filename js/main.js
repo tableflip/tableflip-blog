42a95047bb0815a704a0fcede040e5704f4b6955
Alan Shaw <alan@freestyle-developments.co.uk>
2014-01-22 12:27:32 +0000

    Remove old site files

--- js/main.js
+++
@@ -1,9 +0,0 @@
-jQuery(function ($) {
-  $("#main").css("min-height", $(window).height())
-  
-  if (window.location.hash == "#sent") {
-    $("#hire-us").hide()
-  } else {
-    $("#sent").hide()
-  }
-})
--- sendmail.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!isset($_POST['email']) || !isset($_POST['msg'])) {
-  header('Location: /#error');
-  exit;
-}
-mail('bot@tableflip.io', 'Contact form submission', $_POST['email']."\n".$_POST['msg']);
-header('Location: /#sent');
